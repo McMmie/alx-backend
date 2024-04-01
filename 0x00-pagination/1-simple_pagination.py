f1ece1090835ac8897448d9dcc770e343a740797
McMmie <tharausamuel01@gmail.com>
2024-04-01 16:47:12 +0000

    optimized function

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""
+"""
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list
+    for those particular pagination parameters.
+    """
+
+    return ((page * page_size) - page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """return a tuple of size two containing a start index and an end index
+        corresponding to the range of indexes to return in a list
+        for those particular pagination parameters.
+        """
+
+        return ((page * page_size) - page_size, page * page_size)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert type(page) == int
+        assert type(page_size) == int
+        assert page >= 0
+        assert page_size >= 0
+        result = []
+        _range = index_range(page, page_size)
+        data = self.dataset()
+
+        if _range[1] <= len(data):
+
+            result.extend(items for items in data[_range[0]:_range[1]])
+            # print(f'data: {data[0]}')
+
+        return result
