8d97c3bb551d8bd494cba5b02d20248ade5c1d85
McMmie <tharausamuel01@gmail.com>
2024-04-03 22:17:01 +0000

    fifo caching replacement policy

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+first in first out caching replacement policy
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    inherits from BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        """Initialize """
+
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        assign to the dictionary self.cache_data the item value for the key
+        """
+        if key and item:
+
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+
+                for index, value in enumerate(self.cache_data.keys()):
+
+                    if index == 0:
+                        discarded = value
+
+                print(f'DISCARDED: {discarded}')
+                self.cache_data.pop(discarded)
+
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key
+        """
+
+        return self.cache_data.get(key)
